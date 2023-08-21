ed7270bb747fff7f9cf0ecee4f7929a25e449fd2
Sven <chanck.mds@gmail.com>
2023-08-21 04:19:12 +0200

    ex 14 is done

---
+++ JavaScript/Array Methods Advanced 4/exercise/exercise.js
@@ -0,0 +1,66 @@
+function uncompletedNotes(notes) {
+  const incompleteNotes = [];
+
+  for (const note of notes) {
+    const incompleteTodos = note.todos.filter(todo => !todo.done);
+    
+      incompleteNotes.push({
+        ...note,
+        todos: incompleteTodos,
+      });
+  }
+
+  return incompleteNotes;
+}
+
+const notes = [
+  {
+    id: 1,
+    description: "Workout program",
+    todos: [
+      {
+        id: 1,
+        name: "Push ups - 10 x 3",
+        done: false,
+      },
+      {
+        id: 2,
+        name: "Abdominals - 20 x 3",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Tapis Roulant - 15min",
+        done: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    description: "Front-end Roadmap",
+    todos: [
+      {
+        id: 1,
+        name: "Learn HTML",
+        done: true,
+      },
+      {
+        id: 2,
+        name: "Learn CSS",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Learn JavaScript",
+        done: true,
+      },
+      {
+        id: 4,
+        name: "Learn Angular",
+        done: false,
+      },
+    ],
+  },
+];
+
+console.log(uncompletedNotes(notes));

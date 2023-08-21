1913e2e897101d8836a2a312b04894ee6b154116
Sven <chanck.mds@gmail.com>
2023-08-21 03:03:57 +0200

    ex 7 is done

---
+++ JavaScript/Closures 2/exercise/exercise.js
@@ -0,0 +1,12 @@
+function printName() {
+    const helloName = "Hello John";
+  
+    function inner() {
+      return helloName;
+    }
+    setTimeout(() => {
+      console.log(inner())
+    }, 1000);
+    
+  }
+printName();  

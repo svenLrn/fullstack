cc7a631f2c2209490168770530e45d421e952539
Sven <chanck.mds@gmail.com>
2023-08-21 02:36:05 +0200

    ex 3 is done

---
+++ JavaScript/Arrow Functions/exercise/exercise.js
@@ -0,0 +1,8 @@
+const sum = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const log = value => console.log(value);
+
+const result = divide(subtract(multiply(sum(2, 4), sum(5, 2)), 2), 5);
+log(result);

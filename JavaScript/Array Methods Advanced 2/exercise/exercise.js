e6d3a5b51ca6b0356cb3246084e418df848e45c7
Sven <chanck.mds@gmail.com>
2023-08-21 03:45:45 +0200

    ex 12 is done

---
+++ JavaScript/Array Methods Advanced 2/exercise/exercise.js
@@ -0,0 +1,26 @@
+function nicknameMap(persons) {
+  let i = 0;
+  let nicksArray = [];
+  persons.forEach(person => {
+    nicksArray[i] = person.name + '-' + person.age;
+    i+=1;
+  });
+  return nicksArray;
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const nicknames = nicknameMap(persons);
+console.log(persons);
+console.log(nicknames);

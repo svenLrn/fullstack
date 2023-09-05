e9bb77093b55d2a2f734aef99d6581f41ecd3060
Sven <chanck.mds@gmail.com>
2023-09-05 22:42:19 +0200

    ex 22 is done

---
+++ JavaScript/Destructuring 2/exercise/exercise.js
@@ -0,0 +1,11 @@
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+const [id, firstName, lastName, age] = [person.id, person.firstName, person.lastName, person.age]
+
+
+console.log(id, firstName, lastName, age);

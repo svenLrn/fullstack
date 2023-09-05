fd8a4a2b8a904f55d731c3d1895fccc85e2a79c8
Sven <chanck.mds@gmail.com>
2023-09-05 11:24:46 +0200

    ex 20 is done

---
+++ JavaScript/instanceof Operator/exercise/exercise.js
@@ -0,0 +1,38 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+  static calculate(figure) {
+    if (figure instanceof Square) {
+      return figure.side ** 2;
+    } else if (figure instanceof Rectangle) {
+      return figure.width * figure.height;
+    } else {
+      return Math.PI * figure.radius ** 2;
+    }
+  }
+}
+
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle));
